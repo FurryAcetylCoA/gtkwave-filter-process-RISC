85f5a8925cb86e8bfa111aeb4bcada744d55ee91
CoA <1109673069@qq.com>
2024-03-24 19:59:46 +0800

    wip

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CXX = clang++
 
-CXXFLAGS += $(shell llvm-config --cxxflags) -O3 -flto
-LIBS += $(shell llvm-config --libs)
-LDFLAGS += $(shell llvm-config --ldflags)
+CXXFLAGS += -O3 -flto
+LIBS += -Wl,-Bstatic -lm -lcapstone -Wl,-Bdynamic
+LDFLAGS += $(shell llvm-config --ldflags) -L.
 LDFLAGS += $(shell if command -v mold --version >/dev/null 2>&1; then echo "-fuse-ld=mold"; fi)
 SRCS = $(wildcard src/*.cpp)
 BUILDDIR = bin
--- src/disasm.cpp
+++ src/disasm.cpp
@@ -14,104 +14,26 @@
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
-#if defined(__GNUC__) && !defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#endif
+#include <capstone/capstone.h>
+#include <string>
+#include <iostream>
+#include <sstream>
 
-#include "llvm/MC/MCAsmInfo.h"
-#include "llvm/MC/MCContext.h"
-#include "llvm/MC/MCDisassembler/MCDisassembler.h"
-#include "llvm/MC/MCInstPrinter.h"
-#if LLVM_VERSION_MAJOR >= 14
-#include "llvm/MC/TargetRegistry.h"
-#if LLVM_VERSION_MAJOR >= 15
-#include "llvm/MC/MCSubtargetInfo.h"
-#endif
-#else
-#include "llvm/Support/TargetRegistry.h"
-#endif
-#include "llvm/Support/TargetSelect.h"
-
-#if defined(__GNUC__) && !defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
-
-#if LLVM_VERSION_MAJOR < 11
-#error Please use LLVM with major version >= 11
-#endif
-
-using namespace llvm;
-
-static llvm::MCDisassembler *gDisassembler = nullptr;
-static llvm::MCSubtargetInfo *gSTI = nullptr;
-static llvm::MCInstPrinter *gIP = nullptr;
+static csh handle;
 
 void init_disasm(std::string triple) {
-  llvm::InitializeAllTargetInfos();
-  llvm::InitializeAllTargetMCs();
-  llvm::InitializeAllAsmParsers();
-  llvm::InitializeAllDisassemblers();
-
-    std::string errstr;
-
-    llvm::MCInstrInfo *gMII = nullptr;
-    llvm::MCRegisterInfo *gMRI = nullptr;
-    auto target = llvm::TargetRegistry::lookupTarget(triple, errstr);
-    if (!target) {
-        llvm::errs() << "Can't find target for " << triple << ": " << errstr
-                     << "\n";
-        assert(0);
-    }
-
-    MCTargetOptions MCOptions;
-    gSTI = target->createMCSubtargetInfo(triple, "", "");
-    std::string isa = target->getName();
-    if (isa == "riscv32" || isa == "riscv64") {
-    	gSTI->ApplyFeatureFlag("+m");
-    	gSTI->ApplyFeatureFlag("+a");
-    	// gSTI->ApplyFeatureFlag("+c");
-    	gSTI->ApplyFeatureFlag("+f");
-    	gSTI->ApplyFeatureFlag("+d");
-    }else if(isa == "loongarch32"){
-
-    }
-    gMII = target->createMCInstrInfo();
-    gMRI = target->createMCRegInfo(triple);
-    auto AsmInfo = target->createMCAsmInfo(*gMRI, triple, MCOptions);
-#if LLVM_VERSION_MAJOR >= 13
-    auto llvmTripleTwine = Twine(triple);
-    auto llvmtriple = llvm::Triple(llvmTripleTwine);
-    auto Ctx = new llvm::MCContext(llvmtriple, AsmInfo, gMRI, nullptr);
-#else
-    auto Ctx = new llvm::MCContext(AsmInfo, gMRI, nullptr);
-#endif
-    gDisassembler = target->createMCDisassembler(*gSTI, *Ctx);
-    gIP = target->createMCInstPrinter(llvm::Triple(triple),
-                                      AsmInfo->getAssemblerDialect(), *AsmInfo,
-                                      *gMII, *gMRI);
-    gIP->setPrintImmHex(true);
-    gIP->applyTargetSpecificCLOption("no-aliases");
+    cs_open(CS_ARCH_RISCV, CS_MODE_RISCV32, &handle);
 }
 
 std::string disassemble(uint64_t hx) {
     uint8_t *code = reinterpret_cast<uint8_t *>(&hx);
-    MCInst inst;
-    llvm::ArrayRef<uint8_t> arr(code, 4);
-    uint64_t dummy_size = 0, addr = 0;
-    std::string s;
-    raw_string_ostream os(s);
-    if(gDisassembler->getInstruction(inst, dummy_size, arr, addr, llvm::nulls()) != MCDisassembler::Success){
-        os << "invalid inst:" << llvm::format_hex_no_prefix(hx,8);
-        return s;
+    cs_insn *insn;
+    std::ostringstream oss;
+    int count = cs_disasm(handle, code, sizeof(uint32_t), 0, 1, &insn);
+    if(!count){
+        std::cout<<"Failed"<<std::endl;
     }
-    gIP->printInst(&inst, addr, "", *gSTI, os);
 
-    // Assume result format "\tOpName\tOperands"
-    size_t tabPosition = s.find("\t");
-    while (tabPosition != std::string::npos) {
-        s.replace(tabPosition, 1, " ");
-        tabPosition = s.find("\t", tabPosition + 1);
-    }
-    return s.substr(1);
+    oss<<insn->mnemonic<<" "<<insn->op_str<<std::endl;
+    return oss.str();
 }
